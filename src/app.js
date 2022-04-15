e0af387bdc14829adb0bb291b420215232675d45
return1 <lorieet@gmail.com>
2022-04-15 13:34:48 -0300

    modificada la estructura de carpetas y la dirección de public en node

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.listen(3000, () => {
     console.log("El servidor de prendio en el puerto:3000");
 });
