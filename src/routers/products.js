bfbfa8e5ad0d9e9e0bd36a3f481b0123b762d7e8
scisneros <santi.cisneros@live.com.ar>
2022-05-06 20:02:00 -0300

    instalando multer, se crea producto de prueba y funciona. falta guardar la ruta en la DB

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,12 +1,27 @@
 const { Router } = require("express");
 const productsController = require("../controllers/products-controller");
-
+const path = require("path");
 const router = Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/img/multer');
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+    }
+})
+
+const uploadFile = multer({ storage });
+
+
+
 
 // /products 
 router.get("/", productsController.index);
 router.get("/create", productsController.create);
-router.post("/create", productsController.store)
+router.post("/create", uploadFile.single("img"), productsController.store)
 router.get("/edit/:id", productsController.edit);
 router.put("/edit/:id", productsController.update);
 router.delete("/delete/:id", productsController.destroy);
