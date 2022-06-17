04a8f2b942073871d9e2261841a1bf020d9e0ee4
return1 <lorieet@gmail.com>
2022-06-17 13:07:01 -0300

    agregado el model 'BuyDetail'

---
+++ src/database/models/BuyDetail.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, dgitataTypes) => {
+    let alias = "BuyDetail";
+    let cols = {
+      id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_id: {
+        type: dataTypes.INTEGER,
+      },
+      quantity: {
+        type: dataTypes.INTEGER,
+      },
+      user_id: {
+        type: dataTypes.INTEGER,
+      },
+      operation_id: {
+        type: dataTypes.INTEGER,
+      },
+      price: {
+        type: dataTypes.FLOAT,
+      }
+    };
+    let config = {
+        tableName: "buyDetail",
+        timestamps: false
+    }
+
+    const buyDetail = sequelize.define(alias, cols, config);
+
+    return buyDetail;
+}
