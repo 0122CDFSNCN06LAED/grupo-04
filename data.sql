020972915d83c9af2221e7909cd65371035a79a7
return1 <lorieet@gmail.com>
2022-06-22 20:52:23 -0300

    corregido camel case de data.sql

--- data.sql
+++ data.sql
@@ -10,17 +10,17 @@ insert into brands (id, name) values
 (9, 'Izio'),
 (10, 'Skinix');
 
-insert into productcategories (id, name, description) values (1, 'almacen', 'blablablabla'),
+insert into productCategories (id, name, description) values (1, 'almacen', 'blablablabla'),
  (2, 'Herramientas', 'blablablabla'),
  (3, 'libreria', 'blablablabla'),
  (4, 'muebles', 'blablablabla'),
  (5, 'tecnologia', 'blablablabla'),
  (6, 'Vestimenta', 'blablablabla');
  
- insert into usercategories (id, type) values (1, 'admin'),
+ insert into userCategories (id, type) values (1, 'admin'),
 (2, 'normal');
 
-insert into productsubcategories (id, name, description, productCategory_id) values (1, 'nombre de subcategoria', 'blablablabla', 6),
+insert into productSubCategories (id, name, description, productCategory_id) values (1, 'nombre de subcategoria', 'blablablabla', 6),
  (2, 'nombre de subcategoria', 'blablablabla', 3),
  (3, 'nombre de subcategoria', 'blablablabla', 6),
  (4, 'nombre de subcategoria', 'blablablabla', 2),
@@ -114,7 +114,7 @@ insert into products (id, productCategory_id, productSubCategory, productName, p
  (29, 1, 5, 'Nombre del Producto026', 84660, 13, 'http://dummyimage.com/146x100.png/ff4444/ffffff', 5),
  (30, 2, 2, 'Nombre del Producto200', 66883, 10, 'http://dummyimage.com/203x100.png/cc0000/ffffff', 2);
  
- insert into users (id, userName, firstName, lastName, email, cuit, companyName, phoneNumber, password, companyImg, usercategory_id) values  
+ insert into users (id, userName, firstName, lastName, email, cuit, companyName, phoneNumber, password, companyImg, userCategory_id) values  
  (1, 'cblasgen0', 'Caty', 'Blasgen', 'cblasgen0@columbia.edu', 828437021, 'Rodriguez, Bednar and Bergstrom', 615252920, 'N1USyJKtQ3cm', 'http://dummyimage.com/216x100.png/ff4444/ffffff', 2),
  (2, 'mmackinder1', 'Milt', 'Mackinder', 'mmackinder1@cdc.gov', 925814942, 'Runolfsson-Padberg', 280428797, '9VyMSEp3', 'http://dummyimage.com/163x100.png/cc0000/ffffff', 2),
  (3, 'nredwing2', 'Noll', 'Redwing', 'nredwing2@apache.org', 795591695, 'Dare, Dietrich and Langosh', 568287395, 'yP3Z5J9TB42x', 'http://dummyimage.com/242x100.png/ff4444/ffffff', 2),
@@ -147,7 +147,7 @@ insert into products (id, productCategory_id, productSubCategory, productName, p
  (30, 'rmarchingtont', 'Ryon', 'Marchington', 'rmarchingtont@twitter.com', 627527342, 'Boehm LLC', 634717067, 'FX48i5z', 'http://dummyimage.com/123x100.png/5fa2dd/ffffff', 2);
 
  
- insert into products_productcategories (id, product_id, productCategories_id) values (1, 1, 4),
+ insert into products_productCategories (id, product_id, productCategories_id) values (1, 1, 4),
  (2, 2, 2),
  (3, 3, 3),
  (4, 4, 2),
@@ -178,7 +178,7 @@ insert into products (id, productCategory_id, productSubCategory, productName, p
  (29, 29, 2),
  (30, 30, 2);
  
- insert into buydetail (id, product_id, quantity, user_id, operation_id, price) values (1, 24, 39, 5, 1, 782185),
+ insert into buyDetail (id, product_id, quantity, user_id, operation_id, price) values (1, 24, 39, 5, 1, 782185),
  (2, 14, 52, 29, 2, 97377),
  (3, 12, 57, 14, 3, 308657),
  (4, 10, 20, 1, 4, 489065),
@@ -187,7 +187,7 @@ insert into products (id, productCategory_id, productSubCategory, productName, p
  (7, 23, 8, 16, 7, 996158),
  (8, 19, 21, 14, 8, 409312);
  
- insert into favoriteproducts (id, product_id, user_id) values (1, 19, 26),
+ insert into favoriteProducts (id, product_id, user_id) values (1, 19, 26),
 (2, 24, 30),
 (3, 19, 23),
 (4, 21, 9),
@@ -198,7 +198,7 @@ insert into products (id, productCategory_id, productSubCategory, productName, p
 (9, 5, 29),
 (10, 27, 29);
 
-insert into productcar (id, product_id, quantity, user_id) values (1, 16, 3, 13),
+insert into productCar (id, product_id, quantity, user_id) values (1, 16, 3, 13),
 (2, 9, 2, 20),
 (3, 9, 3, 3),
 (4, 27, 5, 7),
