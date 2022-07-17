5580cbb94ec1ebe8dca64b2020406bd8540b8921
Ricardo Castro Lloret <lorieet@gmail.com>
2022-07-17 18:03:37 -0300

    removido model/users del codigo, pero no el archivo

--- src/middlewares/usuarioLogeado.js
+++ src/middlewares/usuarioLogeado.js
@@ -1,21 +1,19 @@
- const users = require('../models/user')
+ //const users = require('../models/user')
 
 function usuarioLogeado(req, res, next) {
 
 
     res.locals.isLogged = false;
-    let emailEnCookie = req.cookies.userEmail
-    let userEnCookie = users.findByField('email', emailEnCookie)
+    let emailEnCookie = req.cookies.userEmail;
+    let userEnCookie = null//users.findByField('email', emailEnCookie)
 
     if(userEnCookie){
         req.session.userLogged = userEnCookie
-        console.log("si hay user en cookie: ", userEnCookie)
     }
 
     if (req.session.userLogged) {
         res.locals.isLogged = true;
         res.locals.userLogged = req.session.userLogged;
-        console.log("si NO hay user en cookie: ",userEnCookie)
     }
     
     next();
