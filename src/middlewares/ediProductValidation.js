d96fec4385da3eb6d93627c7958b4ed5566209ae
scisneros <santi.cisneros@live.com.ar>
2022-07-20 13:50:27 -0300

    se le terminan de agregar msg a form de productos

--- src/middlewares/ediProductValidation.js
+++ src/middlewares/ediProductValidation.js
@@ -4,11 +4,25 @@ const { body } = require('express-validator');
 const validations = [
     body('productName').notEmpty().withMessage('Escriba un nombre'),
     body('productName').isLength({ min: 5 }).withMessage('minimo 5 caracteres'),
-    body('description').notEmpty().isLength({ min: 20 }),
+    body('description').notEmpty().isLength({ min: 20 }).withMessage('agregue una descripcion'),
+    body('description').isLength({ min: 20 }).withMessage('La descripcion debe ser superior a los 20 caracteres'),
     body('price').notEmpty().withMessage('Escriba el precio del producto').bail()
     .isNumeric().withMessage('Ingrese un numero'),
     body('minBuy').notEmpty().withMessage('Escriba el minimo de compra').bail()
     .isNumeric().withMessage('Ingrese un numero'),
+    body('productImages').custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', 'gif'];
+        if (!file) {
+            throw new Error('Suba una imagen');
+        } else {
+            let fileExtensions = path.extname(file.originalname);
+            if (acceptedExtensions.includes(fileExtensions) === false) {
+                throw new Error('Los archivos permitidos son ' + acceptedExtensions.join(', '))
+            }
+        };
+        return true;
+    }),
 ]
 
 
