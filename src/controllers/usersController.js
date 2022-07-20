9570cf1b519ad0be131ad4f7d5bbe8bedfd8f13a
Ricardo Castro Lloret <lorieet@gmail.com>
2022-07-20 20:07:59 -0300

    modificado productController

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,11 +1,11 @@
 const res = require("express/lib/response");
-const fs = require("fs");
-const path = require("path");
+//const fs = require("fs");
+//const path = require("path");
 const bcrypt = require("bcryptjs");
 let db = require("../database/models")
 
-const usersFilePath = path.join(__dirname, "../data/users.json");
-const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+//const usersFilePath = path.join(__dirname, "../data/users.json");
+//const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 
 
