f040b4bc7ccb45275bb3cb735676f330de4650dc
Ricardo Castro Lloret <lorieet@gmail.com>
2022-07-25 18:36:21 -0300

    modificado data.sql

--- data.sql
+++ data.sql
@@ -48,7 +48,7 @@ insert into productCategories (id, name, description) values
 (17, 8, 'Fan ', 'descripcion de modelo');
 
 insert into users (id, userName, firstName, lastName, email, cuit, companyName, phoneNumber, password, companyImg, userCategory_id) values  
- (1, 'admin@admin.com', 'admin', 'admin', 'admin@admin.com', 828437021, 'BMP', 615252920, '$2a$10$3JRBBvulx7pqYfMP8vqE.eR7YNBEqSPk4kqDTWu9qCFZy/gki3SyO', 'default-avatar.png', 2);
+ (1, 'admin@admin.com', 'admin', 'admin', 'admin@admin.com', 828437021, 'BMP (admin)', 615252920, '$2a$10$3JRBBvulx7pqYfMP8vqE.eR7YNBEqSPk4kqDTWu9qCFZy/gki3SyO', 'default-avatar.png', 2), (2, 'user@user.com', 'user', 'user', 'user@user.com', 828437021, 'BMP (user)', 615252920, '$2a$10$3JRBBvulx7pqYfMP8vqE.eR7YNBEqSPk4kqDTWu9qCFZy/gki3SyO', 'default-avatar.png', 2);
 
 insert into products (id, productName, price, minBuy, productImages, description, models_id, category_id,vendor_id) values 
  (1, 'Azucar', 300, 10, 'azucar.jpg', 'azucar de primera calidad', 1, 1, 1),
@@ -57,10 +57,10 @@ insert into products (id, productName, price, minBuy, productImages, description
  (4, 'Sierra Sensitiva ', 88520, 5, 'sierraSensitiva.jpg', 'Bosch Gco 220 2200w 3800rpm 355mm 1 Disco', 7, 2, 1),
  (5, 'Escritorio', 98424, 10, 'escritorioOficina.webp', 'escritorio pc Su-Office Fan melamina de 163cm x 75cm x 60cm x 140cm blanco y negro', 17, 4, 1),
  (6, 'Cajonera', 30324, 5, 'cajonera.webp', '4 Cajones / Escritorio / Oficina Con Ruedas', 3, 4, 1),
- (7, 'Impresora', 150124, 5, 'impresora.jpg','color multifunción Epson EcoTank L3210 negra 220V', 4, 5, 1),
- (8, 'Impresora', 13255, 5, 'impresoraXerox.jpg', 'Impresora simple función Xerox Phaser 3020/BI con wifi blanca y azul 220V - 240V', 8, 5, 1),
+ (7, 'Impresora', 150124, 5, 'impresora.jpg','color multifunción Epson EcoTank L3210 negra 220V', 4, 5, 2),
+ (8, 'Impresora', 13255, 5, 'impresoraXerox.jpg', 'Impresora simple función Xerox Phaser 3020/BI con wifi blanca y azul 220V - 240V', 8, 5, 2),
  (9, 'Apple Macbook Air', 300524, 2, 'macbook1.webp','computadora Apple primera generacion', 9, 5, 2), 
- (10, 'Monitor LG 24MK430H', 150909, 10, 'monitorLg.jpeg','led 23.8 negro 100V/240V', 10, 5, 1),
+ (10, 'Monitor LG 24MK430H', 150909, 10, 'monitorLg.jpeg','led 23.8 negro 100V/240V', 10, 5, 2),
  (11, 'Delantal', 5200, 30, 'delantalCocina.jpg', 'Delantal de cocina Universal', 5, 6, 1),
  (12, 'Remera Clásica', 4000, 10, 'remeraLisa.jpg', 'Remera Clásica Manga Corta Blanca', 16, 6, 1),
  (13, 'Resma A4 Papel Autor 75grs 500 Hojas', 2500, 4, 'cajaHojasA4.jpg','Papel Autor 75grs 500 Hojas', 14, 3, 1),
