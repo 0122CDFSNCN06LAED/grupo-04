0817839ac934bfa491937ebbb3881d065187447b
scisneros <santi.cisneros@live.com.ar>
2022-07-31 15:40:58 -0300

    se genera un mensaje de conf antes de eliminar un producto

--- backend/public/js/eliminarProducto.js
+++ backend/public/js/eliminarProducto.js
@@ -1,10 +1,22 @@
 window.addEventListener("load",function(){
 
+    let botonEliminar =document.getElementById("cuidadoAlEliminar");
+    console.log(botonEliminar, "te elimino");
 
-
+    botonEliminar.addEventListener('click',function(e){
+       
+         
+         if(confirm("¿seguro que deseas eliminar este prducto?")== true){
+         }
+         else{
+            e.preventDefault()
+         }
+    })
 
 })
 ;
+
+
 
 
 
